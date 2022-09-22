0fdd04c5b33bfa631e66044ebb14a1320e204427
Anton Vibæk <55436877+Svendpai@users.noreply.github.com>
2022-09-22 12:05:19 +0200

    lecture03

--- lecture03-exercises/producer-client/example.py
+++ lecture03-exercises/producer-client/example.py
@@ -1,5 +1,11 @@
-# input
-string = str(input())
-  
-# output
-print(string)
+while True:
+
+    # input
+    input_string = str(input())
+
+    # output
+    from kafka import KafkaProducer
+    import time
+    producer = KafkaProducer(bootstrap_servers='kafka:9092')
+
+    producer.send('foo', value=(input_string.encode('utf-8')))
